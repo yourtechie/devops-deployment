7dc89dcdbb365d2654ae99805753dfdbcef09085
yourtechie <brookeyajayi55@gmail.com>
2026-04-18 12:08:18 +0100

    Test CI/CD

--- charitize/index.php
+++ charitize/index.php
@@ -208,7 +208,7 @@
         <div class="modal-dialog">
             <div class="modal-content rounded-0">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Youtube Video</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Youtube Videos</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
